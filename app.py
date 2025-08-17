2f03006b096a1f9f54fb621255a130a327a72fe5
OMKAR JAGTAP <72138819+omijagtap@users.noreply.github.com>
2025-08-17 22:22:39 +0530

    first commit

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ import smtplib
 from email.mime.text import MIMEText
 from io import BytesIO
 
+import os
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
 from flask import (
     Flask, render_template, request, redirect,
     url_for, send_file, abort, flash, session, make_response
